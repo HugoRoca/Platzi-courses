7513a45f3b6688aa7537dc4901e7758bf8eb9d5e
hugoroca <hugo.rock20@hotmail.com>
2021-12-28 11:34:35 -0500

    [JAVASCRIPT-DATA-STRUCTURE] - class array v1

---
+++ javascript-data-structure/src/array.js
@@ -0,0 +1,19 @@
+// const array = [ "Hugo", "Candy", "Cutu", "Capitan"]
+// array.push("Tobias")
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+}
