f8b90d71dd455c38d9fdded74dcd3b71b1ff0aa5
hugoroca <hugo.rock20@hotmail.com>
2021-12-30 15:49:32 -0500

    [JAVASCRIPT-DATA-STRUCTURE] - trees

---
+++ javascript-data-structure/src/tree/tree.js
@@ -0,0 +1,48 @@
+//       10
+//   4        20
+// 2   8   17    170
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  // search with value and return all nodes
+}
+
+const myTree = new BinarySearchTree();
