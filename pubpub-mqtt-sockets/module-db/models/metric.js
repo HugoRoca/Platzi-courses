33200bb56e0b803035f8195e5d9fd78d2bae1005
hugoroca <hugo.rock20@hotmail.com>
2022-01-26 23:11:00 -0500

    [PUBSUB-MQTT-SOCKETS] - define foreing keys

--- pubpub-mqtt-sockets/module-db/models/metric.js
+++ pubpub-mqtt-sockets/module-db/models/metric.js
@@ -1,17 +1,17 @@
 const Sequelize = require('sequelize')
 const setupDatabase = require('../lib/db')
 
-module.exports = function setupMetricModel (config) {
+module.exports = function setupMetricModel(config) {
   const sequelize = setupDatabase(config)
 
   return sequelize.define('metric', {
     type: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
     },
     value: {
       type: Sequelize.TEXT,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   })
 }
