05a633954269288e0f2f90dccad00f5b39ae66b4
hugoroca <hugo.rock20@hotmail.com>
2022-01-27 23:31:02 -0500

    [PUBSUB-MQTT-SOCKETS] - implements setup sequelize

--- pubpub-mqtt-sockets/module-db/index.js
+++ pubpub-mqtt-sockets/module-db/index.js
@@ -12,6 +12,10 @@ module.exports = async function (config) {
 
   await sequelize.authenticate()
 
+  if (config.setup) {
+    await sequelize.sync({ force: true })
+  }
+
   const Agent = {}
   const Metric = {}
 
---
+++ pubpub-mqtt-sockets/module-db/setup.js
@@ -0,0 +1,45 @@
+const debug = require('debug')('module:db:setup')
+// const chalk = require('chalk')
+const inquirer = require('inquirer')
+
+const db = require('./index')
+
+const prompts = inquirer.createPromptModule()
+
+async function setup() {
+  const answer = await prompts([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?',
+    },
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+
+  const config = {
+    database: process.env.DB_NAME || 'pubsub-mqtt-socket',
+    username: process.env.DB_USER || 'hugo',
+    password: process.env.DB_PASS || 'admin123',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '5432',
+    dialect: 'postgres',
+    logging: (s) => debug(s),
+    setup: true,
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+}
+
+function handleFatalError(err) {
+  // console.error(`${chalk.red(err.message)}`)
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+setup()
