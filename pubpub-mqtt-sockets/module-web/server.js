a0502b02d4da936ad4100b14c0ca9883637f7664
hugoroca <hugo.rock20@hotmail.com>
2022-02-03 18:21:53 -0500

    [PUBSUB-MQTT-SOCKETS] - implements sockets methods

---
+++ pubpub-mqtt-sockets/module-web/server.js
@@ -0,0 +1,38 @@
+const debug = require('debug')('module:web')
+const http = require('http')
+const express = require('express')
+const chalk = require('chalk')
+const path = require('path')
+const socketio = require('socket.io')
+
+const port = process.env.PORT || 8080
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Socket.io / WebSockets
+io.on('connect', (socket) => {
+  debug(`Connected ${socket.id}`)
+  socket.on('agent/message', (payload) => {
+    console.log(payload)
+  })
+
+  setInterval(() => {
+    socket.emit('agent/message', { agent: 'xxx-yyy' })
+  }, 2000)
+})
+
+function handleFatalError(err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[module-web]')} server listening on port ${port}`)
+})
