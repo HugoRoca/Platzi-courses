afeaedce3383e0ad66f4e665c5bd3088150a777d
hugoroca <hugo.rock20@hotmail.com>
2022-02-05 23:50:21 -0500

    [PUBSUB-MQTT-SOCKETS] - implements cli with agents

--- pubpub-mqtt-sockets/module-cli/module.js
+++ pubpub-mqtt-sockets/module-cli/module.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const ModuleAgent = require('module-agent')
 
 const screen = blessed.screen()
+const agent = new ModuleAgent()
+
+const agents = new Map()
+const agentsMetrics = new Map()
 
 const grid = new contrib.grid({
   rows: 1,
@@ -21,8 +26,40 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5,
 })
 
+agent.on('agent/connected', (payload) => {
+  const { uuid } = payload.agent
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentsMetrics.set(uuid, {})
+  }
+
+  renderData()
+})
+
+function renderData() {
+  const treeData = {}
+
+  for (const [uuid, val] of agents) {
+    const title = `${val.name} - (${val.pid})`
+    treeData[title] = {
+      uuid,
+      agent: true,
+      children: {},
+    }
+  }
+
+  tree.setData({
+    extended: true,
+    children: treeData,
+  })
+
+  screen.render()
+}
+
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
   process.exit(0)
 })
 
+agent.connect()
 screen.render()
