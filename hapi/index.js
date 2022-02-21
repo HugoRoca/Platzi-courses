6ed51dd58a5ad08461e6cf31ebe12a4fe8ba7930
hugoroca <hugo.rock20@hotmail.com>
2022-02-20 21:28:53 -0500

    [HAPI] - implements template register

--- hapi/index.js
+++ hapi/index.js
@@ -48,10 +48,24 @@ async function init() {
       },
     });
 
-    // server.route({
-    //   method: "POST",
-    //   path: "/"
-    // })
+    server.route({
+      method: "GET",
+      path: "/register",
+      handler: (req, h) => {
+        return h.view("register", {
+          title: "Register",
+        });
+      },
+    });
+
+    server.route({
+      method: "POST",
+      path: "/create-user",
+      handler: (req, h) => {
+        console.log(req.payload);
+        return "user created";
+      },
+    });
 
     await server.start();
   } catch (error) {
