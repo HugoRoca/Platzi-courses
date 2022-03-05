18bd1b514ebb04bb29323fae44c1dae728b7a0db
hugoroca <hugo.rock20@hotmail.com>
2022-03-04 23:22:28 -0500

    [NESTJS-TYPEORM] - Entities created

--- nestjs-persistence/src/users/entities/customer.entity.ts
+++ nestjs-persistence/src/users/entities/customer.entity.ts
@@ -1,6 +1,16 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
 export class Customer {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 100 })
   name: string;
+
+  @Column({ type: 'varchar', length: 100 })
   lastName: string;
+
+  @Column({ type: 'varchar', length: 20 })
   phone: string;
 }
