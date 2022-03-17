e6ed9e81c7435861bc5ec99459e9b07e57d40992
hugoroca <hugo.rock20@hotmail.com>
2022-03-16 23:11:25 -0500

    [NESTJS-TYPEORM] - connect to mongoDB with useFactory

--- nestjs-persistence-mongodb/src/database/database.module.ts
+++ nestjs-persistence-mongodb/src/database/database.module.ts
@@ -1,7 +1,16 @@
 import { Module, Global } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
+
+// async function run() {
+//   await client.connect();
+//   const database = client.db('store');
+//   const taskCollection = database.collection('tasks');
+//   const tasks = await taskCollection.find().toArray();
+//   console.log('-> tasks', tasks);
+// }
 
 @Global()
 @Module({
@@ -10,7 +19,17 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'MONGO',
+      useFactory: async () => {
+        const uri =
+          'mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary';
+        const client = new MongoClient(uri);
+        await client.connect();
+        return client.db('store');
+      },
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'MONGO'],
 })
 export class DatabaseModule {}
