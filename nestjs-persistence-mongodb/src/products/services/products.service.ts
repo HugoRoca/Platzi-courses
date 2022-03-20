bb44f38068187f53b6845673aa09b98b62f4882e
hugoroca <hugo.rock20@hotmail.com>
2022-03-20 18:43:40 -0500

    [NESTJS-TYPEORM] - relation one to one

--- nestjs-persistence-mongodb/src/products/services/products.service.ts
+++ nestjs-persistence-mongodb/src/products/services/products.service.ts
@@ -24,7 +24,12 @@ export class ProductsService {
         filters.price = { $gte: minPrice, $lte: maxPrice };
       }
 
-      return this.productModel.find(filters).skip(offset).limit(limit).exec();
+      return this.productModel
+        .find(filters)
+        .populate('brand')
+        .skip(offset)
+        .limit(limit)
+        .exec();
     }
     return this.productModel.find().exec();
   }
