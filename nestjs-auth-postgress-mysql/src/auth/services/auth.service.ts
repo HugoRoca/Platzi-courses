b4e306b7d4cd8a0753ce98f3b3ea1bf0d97c17d8
hugoroca <hugo.rock20@hotmail.com>
2022-03-25 23:45:03 -0500

    [NESTJS-AUTH] - implemetns jwt part 1

--- nestjs-auth-postgress-mysql/src/auth/services/auth.service.ts
+++ nestjs-auth-postgress-mysql/src/auth/services/auth.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 import { UsersService } from '../../users/services/users.service';
+import { User } from '../../users/entities/user.entity';
+import { PayloadToken } from '../models/token.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
@@ -14,5 +20,13 @@ export class AuthService {
     if (user && isMatch) return user;
 
     return null;
+  }
+
+  generateJWT(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
