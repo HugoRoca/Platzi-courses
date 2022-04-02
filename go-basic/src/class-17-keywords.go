2ba793c6a097bd50509e8e89cddd5e5b253409e5
hugoroca <hugo.rock20@hotmail.com>
2022-04-02 00:46:47 -0500

    [GO] - array and slice

--- go-basic/src/class-17-keywords.go
+++ go-basic/src/class-17-keywords.go
@@ -3,22 +3,22 @@ package main
 import "fmt"
 
 func main() {
-  // Defer
-  defer fmt.Println("Hello")
-  fmt.Println("word")
+	// Defer
+	defer fmt.Println("Hello")
+	fmt.Println("word")
 
-  // Continue y break
-  for i := 0; i < 10; i++ {
-    fmt.Println(i)
+	// Continue y break
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
 
-    if i == 2 {
-      fmt.Println("is two")
-      continue
-    }
+		if i == 2 {
+			fmt.Println("is two")
+			continue
+		}
 
-    if i == 8 {
-      fmt.Println("break")
-      break
-    }
-  }
+		if i == 8 {
+			fmt.Println("break")
+			break
+		}
+	}
 }
