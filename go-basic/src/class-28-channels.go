2c486eed2cb0bbe5a8dc523f392233967e9dd142
hugoroca <hugo.rock20@hotmail.com>
2022-04-03 23:47:03 -0500

    [GO] - channels

---
+++ go-basic/src/class-28-channels.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// un conducto que se manejar un tipo de dato
+
+// chan<- enter data
+// <-chan output data
+func say(text string, c chan<- string) {
+  c <- text
+}
+
+func main() {
+  c := make(chan string, 1)
+
+  fmt.Println("Hello")
+
+  go say("Bye", c)
+
+  fmt.Println(<-c)
+}
